771d978dd4db72a5c1ac7bc7c46c331622778007
Manny <mhfernan35277@gmail.com>
2024-02-16 00:10:28 -0700

    adds complete file

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setisInCart] = useState(false)
+  function handleCartClick(){
+    //initialize setter function
+    setisInCart(isInCart => !isInCart)
+  }
+  
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCart ? "remove" : "add"} onClick={handleCartClick}
+      >{isInCart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
